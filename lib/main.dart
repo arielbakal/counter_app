66fc0a96e8950816f07373fbf317fe648262b661
arielbakal <arielbakal.dev@gmail.com>
2022-05-16 10:35:36 -0300

    Test edit

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Veces presionado',
+              'Veces que apretas el boton',
             ),
             Text(
               '$_counter',
